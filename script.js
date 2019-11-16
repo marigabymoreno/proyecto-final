1e4a82b2c1580063e705127f4f8d8934d6d93bfd
maria gabriela moreno <mariagabrielamorenop@gmail.com>
2019-11-16 14:41:16 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let caja= document.querySelector("#search-box input[type=search]");
+let boton= document.querySelector("#search-box button");
+let offset=0;
+let resultados= {};
+
+boton.addEventListener("click", searchResultados);
+
+
+
+caja.addEventListener("keyup", function(event) {
+    if(event.keyCode == 13) { 
+        searchResultados();
+    }
+});
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() == $(document).height() - $(window).height())
+    {
+        searchResultados();
+    }
+});
+
+function searchResultados(){
+
+    let url = "http://api.giphy.com/v1/gifs/search?api_key=2HJEIkNChX5m54XYAqtjQRkQvLUUxDO1&offset="+offset+"&limit=15&q="+encodeURI(caja.value);
+    console.log(url);
+    offset = offset + 15;
+
+    fetch(url).then ((response) => response.json()).then((data) => {
+         resultados = data.data;
+         createInfo();
+
+    });
+}
+
+function createInfo() {
+
+    resultados.forEach(resultado => {
+        const resultados_title = document.createElement("h2");
+        const resultados_midia = document.createElement("img"); 
+        resultados_title.textContent = resultado.title;
+        resultados_midia.src = resultado.images.original.url;
+        document.querySelector("#resultados").appendChild(resultados_midia);    
+    });
+}
+
+
+
